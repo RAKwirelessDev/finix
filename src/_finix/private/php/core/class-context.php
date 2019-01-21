e23345994d7fe9444dea8f6e1c63e684230fe460
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-21 22:28:04 +0800

    config php

--- src/_finix/private/php/core/class-context.php
+++ src/_finix/private/php/core/class-context.php
@@ -1,7 +1,7 @@
 <?php
 
 class Context {
-    private static $DEFAULT_PASSHASH = 'cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e';
+    private static $DEFAULT_PASSHASH = 'disabled';
     private static $AS_ADMIN_SESSION_KEY = 'AS_ADMIN';
 
     private $session;
@@ -15,7 +15,7 @@ class Context {
         $this->request = $request;
         $this->setup = $setup;
 
-        $this->options = Json::load($this->setup->get('CONF_PATH') . '/options.json');
+        $this->options = _CONFIG_;
 
         $this->passhash = $this->query_option('passhash', '');
         $this->options['hasCustomPasshash'] = strcasecmp($this->passhash, Context::$DEFAULT_PASSHASH) !== 0;
