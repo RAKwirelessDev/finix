1a30c722974a1402505839032ef29abd4c3c4d43
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-21 23:22:36 +0800

    Video Preview Plugin: Added some shortcut key handler.

--- src/_finix/public/js/lib/ext/preview/preview-vid.js
+++ src/_finix/public/js/lib/ext/preview/preview-vid.js
@@ -1,4 +1,4 @@
-const {dom} = require('../../util');
+const {dom, isFn} = require('../../util');
 const allsettings = require('../../core/settings');
 const preview = require('./preview');
 
@@ -37,6 +37,7 @@ const load = item => {
     return new Promise(resolve => {
         const $el = dom(tpl)
             .on('loadedmetadata', () => resolve($el))
+            .on('click', () => togglePlay($el[0]))
             .attr('controls', 'controls');
         if (settings.autoplay) {
             $el.attr('autoplay', 'autoplay');
@@ -46,9 +47,78 @@ const load = item => {
     });
 };
 
+const togglePlay = el => {
+    if (el.paused) {
+        el.play();
+    } else {
+        el.pause();
+    }
+};
+
+const movieDelta = 5;
+const volumeDelta = 0.07;
+const keypress = key => {
+    const el = dom('#pv-content-vid')[0];
+    if (!el) {
+        return false;
+    }
+
+    let handled = true;
+    switch(key) {
+        case 37: // left
+            el.currentTime -= movieDelta;
+            break;
+
+        case 39: // right
+            el.currentTime += movieDelta;
+            break;
+
+        case 38: // up
+            el.volume += volumeDelta;
+            break;
+
+        case 40: // down
+            el.volume -= volumeDelta;
+            break;
+
+        case 32: // space
+            togglePlay(el);
+            break;
+
+        case 77: // 'm' -- mute
+            el.muted = !el.muted;
+            break;
+
+        case 70:
+        {
+            if (document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement) {
+                let exitFullscreen = document.exitFullscreen || document.webkitExitFullscreen || document.mozCancelFullScreen;
+                if (isFn(exitFullscreen)) {
+                    exitFullscreen.call(document);
+                } else {
+                    handled = false;
+                }
+            } else {
+                let requestFullscreen = el.requestFullscreen || el.webkitRequestFullscreen || el.mozRequestFullScreen;
+                if (isFn(requestFullscreen)) {
+                    requestFullscreen.call(el);
+                } else {
+                    handled = false;
+                }
+            }
+            break;
+        }
+
+        default:
+            handled = false;
+    }
+
+    return handled;
+}
+
 const init = () => {
     if (settings.enabled) {
-        preview.register(settings.types, load, updateGui);
+        preview.register(settings.types, load, updateGui, keypress);
     }
 };
 
